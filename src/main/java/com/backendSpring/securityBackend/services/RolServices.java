80f91a942c7dcc11b04d675200fa1aa42d9bfd8e
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-11-15 00:25:41 -0500

    Services basic creation

---
+++ src/main/java/com/backendSpring/securityBackend/services/RolServices.java
@@ -0,0 +1,72 @@
+package com.backendSpring.securityBackend.services;
+
+import com.backendSpring.securityBackend.models.Rol;
+import com.backendSpring.securityBackend.repositories.RolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RolServices {
+    @Autowired
+    private RolRepository rolRepository;
+
+    public List<Rol> index(){
+    return (List<Rol>) this.rolRepository.findAll();
+    }
+
+    public Optional<Rol> show(int id){
+    return this.rolRepository.findById(id);
+    }
+
+    public Rol create(Rol newRol){
+    if(newRol.getIdRol() == null){
+        // Name restriction to create a rol
+        if (newRol.getName() != null){
+            return this.rolRepository.save(newRol);
+        }
+        else {
+            // TODO 400 BadRequest
+            return  newRol;
+        }
+    }
+    else {
+        // TODO Validate if exists, 400 BadRequest
+        return newRol;
+    }
+    }
+
+    public Rol update(int id, Rol updatedRol){
+        if(id>0){
+            Optional<Rol> tempRol = this.show(id);
+            if(tempRol.isPresent()){
+                // Name restriction to update
+                if (updatedRol.getName()!=null){
+                    tempRol.get().setName(updatedRol.getName());
+                }
+                // Description restriction to update
+                if (updatedRol.getDescription()!=null){
+                    tempRol.get().setDescription(updatedRol.getDescription());
+                }
+                return this.rolRepository.save(tempRol.get());
+            }
+            else {
+                // TODO 404 NotFound
+                return  updatedRol;
+            }
+        }
+        else {
+            // TODO 400 BadRequest
+            return updatedRol;
+        }
+    }
+
+    public boolean delete(int id){
+        return this.show(id).map( rol -> {
+            this.rolRepository.delete(rol);
+            return true;
+        } ).orElse(false);
+    }
+}
---
+++ src/main/java/com/backendSpring/securityBackend/services/UserServices.java
@@ -0,0 +1,75 @@
+package com.backendSpring.securityBackend.services;
+
+import com.backendSpring.securityBackend.models.User;
+import com.backendSpring.securityBackend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+/**
+ *
+ */
+public class UserServices {
+    @Autowired //Connect correctly
+    private UserRepository userRepository;
+
+    /**
+     *
+     * @return
+     */
+    public List<User> index(){
+        return (List<User>) this.userRepository.findAll();
+    }
+
+    public Optional<User> show(int id){
+        return  this.userRepository.findById(id);
+    }
+
+    public User create(User newUser){
+        if ( newUser.getIdUser() == null){
+            if(newUser.getEmail() != null && newUser.getNickname() != null && newUser.getPassword() != null){
+                return this.userRepository.save(newUser);
+            }else {
+                // TODO 400 BadRequest
+                return  newUser;
+            }
+        }
+        else {
+            // TODO: validate if id exists, 400 BadRequest
+            return newUser;
+        }
+    }
+
+    public User update(int id, User updatedUser){
+        if(id>0){
+            Optional<User> tempUser = this.show(id);
+            if(tempUser.isPresent()){
+                // Update everything except nickname
+                if(updatedUser.getNickname()!=null)
+                    //tempUser.get() -> User
+                    tempUser.get().setNickname(updatedUser.getNickname());
+                if(updatedUser.getPassword()!=null)
+                    tempUser.get().setPassword(updatedUser.getPassword());
+                return this.userRepository.save(tempUser.get());
+            }
+            else {
+                //TODO: 404 NotFound
+                return updatedUser;
+            }
+        }
+        else {
+            // TODO: 400 BadRequest, id <= 0
+            return updatedUser;
+        }
+    }
+
+    public boolean delete(int id){
+        return this.show(id).map( user -> {
+            this.userRepository.delete(user);
+            return true;
+        }).orElse(false);
+    }
+}
