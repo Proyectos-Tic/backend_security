3ecb3c399a9b21a147914c9498a4b7304fe4baf4
Sebastian Gomez <sebastiangomezv98@gmail.com>
2022-11-15 14:05:44 -0500

    Update permission microservices

--- src/main/java/com/backendSpring/securityBackend/services/PermissionServices.java
+++ src/main/java/com/backendSpring/securityBackend/services/PermissionServices.java
@@ -2,6 +2,7 @@ package com.backendSpring.securityBackend.services;
 
 import com.backendSpring.securityBackend.models.Permission;
 import com.backendSpring.securityBackend.repositories.PermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Service
 public class PermissionServices {
 
+    @Autowired
     private PermissionRepository permissionRepository;
 
     /**
